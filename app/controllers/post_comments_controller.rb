a2a6198af7936a45a4823373df4dd11bd4f4b742
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-27 17:15:40 +0900

    [create]ajax_comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,19 +3,19 @@ class PostCommentsController < ApplicationController
 before_action :authenticate_user!
 
 	def create
-		book = Book.find(params[:book_id])
-		comment = PostComment.new(post_comment_params)
-		comment.user_id = current_user.id
-		comment.book_id = book.id
-		comment.save
-		redirect_to book_path(book)
+		@book = Book.find(params[:book_id])
+		@post_comment = PostComment.new(post_comment_params)
+		@post_comment.user_id = current_user.id
+		@post_comment.book_id = @book.id
+		@post_comment.save
+		# redirect_to book_path(book)
 	end
 
 	def destroy
 		book = Book.find(params[:book_id]) #外部体から[:book_id]
-		comment = PostComment.find_by(params[:id]) #コントローラ内なら(params[:id])
-		if comment.user == current_user
-		comment.destroy
+		post_comment = PostComment.find_by(params[:id]) #コントローラ内なら(params[:id])
+		if post_comment.user == current_user
+		post_comment.destroy
 		redirect_to request.referer
 		else
 		redirect_to request.referer

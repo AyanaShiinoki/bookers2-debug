c0ed7b7fed567bacc584158908455e9a977043c4
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-27 18:11:58 +0900

    [fix]comment_ajax

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = User.find(@book.user.id)
     @post_comment = PostComment.new
-    @post_comments = @book.post_comments.all
+    @post_comments = @book.post_comments
 
   end
 
